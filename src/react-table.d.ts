cabb396a881832b7a3c1dd93cc84725ba5437e78
bortolilucas <lucasbortolidev@gmail.com>
2024-11-30 13:15:32 -0300

    feat: add sort to analysis table

---
+++ src/react-table.d.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { FilterFn, RankingInfo, RowData } from '@tanstack/react-table'
+
+declare module '@tanstack/react-table' {
+  // Add fuzzy filter types
+  interface FilterFns {
+    fuzzy: FilterFn<unknown>
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo
+  }
+
+  // Extend meta for columns
+  interface TableColumnMeta {
+    className?: string
+  }
+  interface ColumnMeta<TData extends RowData, TValue> extends TableColumnMeta {}
+}
