2212318ab4ad7efe2aa13221b389c51f774c2d36
bortolilucas <lucasbortolidev@gmail.com>
2025-05-12 19:06:30 -0300

    feat: add updated at to analysis visualization

--- src/app/(protected)/analises/pessoas/consultar/ui.tsx
+++ src/app/(protected)/analises/pessoas/consultar/ui.tsx
@@ -193,9 +193,15 @@ export function SearchPersonAnalysisUI({
               )}
             </Button>
           )}
+          <Button theme="placeholder" size="xxs" disabled shadow="base">
+            Atualizada em{' '}
+            {customDayJs(selectedItem.updated_at).format(
+              'DD/MM/YYYY [às] HH:mm:ss',
+            )}
+          </Button>
           <Button theme="opaque" size="xxs" disabled shadow="base">
             Solicitada em{' '}
-            {customDayJs(selectedItem?.created_at).format(
+            {customDayJs(selectedItem.created_at).format(
               'DD/MM/YYYY [às] HH:mm:ss',
             )}
           </Button>
